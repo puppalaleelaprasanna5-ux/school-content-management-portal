0b9a0c6810388707c33c4dd252b9a44156b9e211
Leela Prasanna <puppalaleelaprasanna5@gmail.com>
2026-07-08 23:06:57 +0530

    feat: finalize database schema

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -3,30 +3,58 @@ import { Request, Response } from "express";
 export const activateSchool = async (
   req: Request,
   res: Response
-) => {
-  res.status(200).json({
-    success: true,
-    message: "School Activation API Working",
-    data: req.body,
-  });
+): Promise<void> => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "School Activation API Working",
+      data: req.body,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
 };
 
 export const login = async (
   req: Request,
   res: Response
-) => {
-  res.status(200).json({
-    success: true,
-    message: "Login API Working",
-  });
+): Promise<void> => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "Login API Working",
+      data: req.body,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
 };
 
 export const me = async (
   req: Request,
   res: Response
-) => {
-  res.status(200).json({
-    success: true,
-    message: "Protected Route Working",
-  });
+): Promise<void> => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "Protected Route Working",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
 };
